913cd7547d803bfe7cd4bab16cfd1f3f94d787f5
yasir690 <yasirsaleemrajput@gmail.com>
2024-03-01 15:32:45 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import Result from './result';
+import axios from 'axios';
+const App = () => {
+  const [search,setSearch]=useState('');
+  const [movie,setMovies]=useState([]);
+
+const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+
+  const getAllMovies=()=>{
+
+    axios.get(APIURL)
+    .then((res)=>{
+      setMovies(res.data.results)
+    })
+  }
+  const getSearchedMovies=()=>{
+axios.get(SEARCHAPI+search)
+.then((res)=>{
+  setMovies(res.data.results)
+})
+  }
+  useEffect(()=>{
+    setMovies([]);
+
+    if(search==""){
+getAllMovies()
+    }
+    else{
+      getSearchedMovies()
+    }
+  },[search]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1 className='text-center text-cyan-400 font-bold text-3xl' style={{textTransform:"capitalize"}}>movie search app</h1>
+      <hr className='w-1/4 m-auto border-cyan-400 border-4 rounded mt-2'/>
+      <div className="max-w-[1240px] shadow-xl min-h-[400px] mx-auto p-3">
+        <input type='text' value={search} onChange={(e)=>setSearch(e.target.value)} className="w-full border border-black rounded text-slate-700 p-4"/>
+       <Result movie={movie}/>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/result.js
@@ -0,0 +1,29 @@
+
+import React from 'react';
+
+const Result = ({movie}) => {
+    const boxes=movie.map((item)=>{
+        return <Box image={item.poster_path} title={item.original_title} rating={item.vote_average}/>
+    })
+  return (
+    <div className='w-full grid md:grid-cols-4 gap-5'>
+        {boxes}
+    </div>
+  )
+}
+
+export default Result;
+
+
+export const Box=(props)=>{
+const IMGPATH="https://image.tmdb.org/t/p/w1280";
+    return (
+        <div className='shadow min-h-[200px] mt-3 pb-1'>
+         <img src={IMGPATH +props.image}/>
+         <div className='flex justify-between items-center px-2'>
+            <span className='text-2xl'>{props.title}</span>
+            <span className='text-xl text-yellow-500 font-bold'>{props.rating}</span>
+         </div>
+        </div>
+    )
+}
